9685d6257075cde5126f6e39df14bf385ea7a3ec
Felipe Castro <78855981+FelipeSoft@users.noreply.github.com>
2022-09-26 10:36:41 -0300

    Add files via upload
    Rock-paper-scissors game created with HTML, CSS and JavaScript.

---
+++ script.js
@@ -0,0 +1,93 @@
+//initial data
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
+
+var warning = '';
+var statusPlaying = false;
+var selectedObject = 0;
+var selectedMachine = 0;
+
+resetGame();
+
+//events
+c('#play').addEventListener('click', startGame);
+c('#reset').addEventListener('click', resetGame);
+cs('.container-info-02 img').forEach(element => {
+    element.addEventListener('click', changeObject)
+});
+
+//functions
+function resetGame(){
+    warning = 'Selecione o objeto e clique no botão "jogar" para iniciar.';
+    statusPlaying = false;
+    c('#play').style.display = 'block';
+    c('.img-area-01 img').src = 'img/paper.webp';
+    c('.object-01').innerHTML = 'Papel';
+    cs('.container-info-02 img.active').forEach(element => {
+        element.classList.remove('active');
+    })
+    c('.container-info-02 img:nth-child(1)').classList.add("active");
+
+    renderInfo();
+}
+
+function startGame(e){
+    e.preventDefault();
+    statusPlaying = true;
+
+    if(statusPlaying){
+        var random = Math.floor(Math.random() * 3);
+        selectedMachine = random;
+        if(selectedMachine === 0){
+            c('.img-area-02 img').src = 'img/paper.webp';
+            c('.object-02').innerHTML = 'Papel';
+        } else if(selectedMachine === 1){
+            c('.img-area-02 img').src = 'img/stone.png';
+            c('.object-02').innerHTML = 'Pedra';
+        } else {
+            c('.img-area-02 img').src = 'img/scissors.jpg';
+            c('.object-02').innerHTML = 'Tesoura';
+        }
+    }
+    checkGame();
+    renderInfo();
+}
+
+function changeObject(e){
+    c('.container-info-02 img.active').classList.remove('active');
+    e.target.classList.add('active');
+    selectedObject = parseInt(e.target.getAttribute('data-object'));
+
+    if(selectedObject === 0){
+        c('.img-area-01 img').src = 'img/paper.webp';
+        c('.object-01').innerHTML = 'Papel';
+    } else if(selectedObject === 1){
+        c('.img-area-01 img').src = 'img/stone.png';
+        c('.object-01').innerHTML = 'Pedra';
+    } else {
+        c('.img-area-01 img').src = 'img/scissors.jpg';
+        c('.object-01').innerHTML = 'Tesoura';
+    }
+}
+
+function renderInfo(){
+    c('#warning').innerHTML = warning;
+}
+
+function checkGame(){
+    cs('.container-info-02 img').forEach(element => {
+        element.classList.remove('active');
+    })
+    c('#play').style.display = 'none';
+    if((selectedObject == 0 && selectedMachine == 1) 
+    || (selectedObject == 1 && selectedMachine == 2) 
+    || (selectedObject == 2 && selectedMachine == 0)){
+        warning = 'Você venceu!';
+    } else if((selectedObject == 1 && selectedMachine == 0) 
+    || (selectedObject == 2 && selectedMachine == 1) 
+    || (selectedObject == 0 && selectedMachine == 2)) {
+        warning = 'A máquina venceu!';
+    } else {
+        warning = 'Jogo empatado!';
+    }
+}
